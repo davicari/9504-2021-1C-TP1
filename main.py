43786a620e362fa32001ccae81a235023299b4a1
gosurataibanez@gmail.com <gosurataibanez@gmail.com>
2021-06-09 15:50:31 -0300

    20210609 15:50 update reporte y codigo

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import pandas
 from matplotlib import pyplot as plt 
 
 
+def A_k(a_k,f_k):
+    return a_k*np.cos(f_k)
+def B_k(a_k,f_k):
+    return -a_k*np.sin(f_k)
 
 def serie_fourier_altura(t,indices,amplitudes,fases,w_0):
     acumulador=0
     for k in indices:
-        acumulador = amplitudes[k]*np.cos(w_0*k * t +fases[k]) + acumulador
+        acumulador = A_k(amplitudes[k],fases[k])*np.cos(w_0*k * t ) + B_k(amplitudes[k],fases[k])*np.sin(w_0* k * t )  + acumulador
     return acumulador
 
 hora = 3600
@@ -20,18 +24,16 @@ omega_0 = (2*np.pi*deltaT)/T
 omega = omega_0 * np.array(t)
 freq = omega * 2*np.pi
 
-n_armonicos_elegidos = 6
+n_armonicos_elegidos = 4
 
 mareas = pandas.read_csv('CO-OPS_8410140_met-2019.csv')
 
 
 
-#print(data)
-
 alturas = mareas['Verified (m)']
-#chajá! calcular el valor medio sacando el valor medio.
 
 alturas_promedio = alturas.mean()
+
 alturas_fft = np.fft.fft(alturas) 
 h_alturas_fft = np.abs(alturas_fft)
 a_alturas_fft = np.angle(alturas_fft)
@@ -43,22 +45,12 @@ h_alturas_fft_promedio = np.average(h_alturas_fft_normalizadas)
 
 #filtro = criterio*np.max(h_alturas_fft) Este criterio es malo, prefiero elegir por el numero de armónicos mas altos. los 5 o 6 mas altos.
 filtro = np.partition(h_alturas_fft_normalizadas.flatten(),-n_armonicos_elegidos)[-n_armonicos_elegidos]
-
 h_alturas_fft_filtrado = np.where(h_alturas_fft_normalizadas <  filtro, 0,h_alturas_fft_normalizadas)
-
-
 indices_elementos_filtrados = np.nonzero(h_alturas_fft_filtrado)[0]
-
 print(indices_elementos_filtrados)
-
 sf_alturas = serie_fourier_altura(t,indices_elementos_filtrados,h_alturas_fft_normalizadas,a_alturas_fft,omega_0)
-
-
-
-plt.plot(t,sf_alturas,'r^',t,alturas,'b-')
+plt.plot(t,sf_alturas,'r-',t,alturas,'b--')
 plt.show()
-
-
 
 # print(filtro)
 # print(omega.size)
