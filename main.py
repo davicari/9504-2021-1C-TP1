80f1b21ee859f508a543240a2c753d989faf35e0
gosurataibanez@gmail.com <gosurataibanez@gmail.com>
2021-06-09 09:08:34 -0300

    09:09

--- main.py
+++ main.py
@@ -10,29 +10,24 @@ deltaT = hora
 criterio = 0.05
 
 t = range(0,int(T/deltaT))
-
 omega_0 = (2*np.pi*deltaT)/T
-
 omega = omega_0 * np.array(t)
-
 freq = omega * 2*np.pi
-
 mareas = pandas.read_csv('CO-OPS_8410140_met-2019.csv')
 
 #print(data)
 
 alturas = mareas['Verified (m)']
-
 #chajá! calcular el valor medio sacando el valor medio.
 
-
+alturas_promedio = alturas.mean()
 alturas_fft = np.fft.fft(alturas) 
-
-h_alturas_fft = np.abs(alturas_fft) /2
-
+h_alturas_fft = np.abs(alturas_fft)
+#Normalizo las alturas usando el valor medio
+h_alturas_fft = h_alturas_fft * (alturas_promedio/h_alturas_fft[0])
 h_alturas_fft_media = np.mean(h_alturas_fft)
-
 h_alturas_fft_promedio = np.average(h_alturas_fft)
+
 
 filtro = criterio*np.max(h_alturas_fft)
 
