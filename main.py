3635902ed739988c9d89fe3cf3095d3ef9dca482
Jorge Moreno <jmoren@gmail.com>
2021-06-12 14:55:43 -0300

    Opcion -s para guardar en un csv todo. Se agrego un archivo de resultados

--- main.py
+++ main.py
@@ -38,26 +38,32 @@ def bk(b_k, f_k):
     return -b_k * np.sin(f_k)
 
 
-def serie_fourier_altura(t, indices, amplitudes, fases, w_0, output):
-    output.writerow(["k", "O_k", "Q_k", "A_k", "B_k"])
+def serie_fourier_altura(t, ind, amp, fases, w_0, output, save_data):
+    if (save_data is True):
+        output.writerow(["k", "O_k", "Q_k", "A_k", "B_k"])
     acc = 0
-    for k in indices:
-        a_k = ak(amplitudes[k], fases[k])
-        b_k = bk(amplitudes[k], fases[k])
-        output.writerow([k, amplitudes[k], fases[k], a_k, b_k])
+    for k in ind:
+        a_k = ak(amp[k], fases[k])
+        b_k = bk(amp[k], fases[k])
+
+        if (save_data is True):
+            output.writerow([k, amp[k], fases[k], a_k, b_k])
 
         acc = (a_k * np.cos(w_0 * k * t)) + (b_k * np.sin(w_0 * k * t)) + acc
 
     return acc
 
 
-def procesar_rango(alturas, n_armonicos):
+def procesar_rango(alturas, n_armonicos, save_data):
     # open files
-    now = int(datetime.now().timestamp())
-    fft_file = open(f'resultados/fft_csv_file_{now}.csv', 'w')
-    sft_file = open(f'resultados/sft_csv_file_{now}.csv', 'w')
-    fft_writer = csv.writer(fft_file)
-    sft_writer = csv.writer(sft_file)
+    fft_writer = None
+    sft_writer = None
+    if (save_data is True):
+        now = int(datetime.now().timestamp())
+        fft_file = open(f'resultados/fft_csv_file_{now}.csv', 'w')
+        sft_file = open(f'resultados/sft_csv_file_{now}.csv', 'w')
+        fft_writer = csv.writer(fft_file)
+        sft_writer = csv.writer(sft_file)
 
     # Parametros
     T = len(alturas)
@@ -72,26 +78,26 @@ def procesar_rango(alturas, n_armonicos):
     a_alturas_fft = np.angle(alturas_fft)
     h_alturas_fft_normalizadas = h_alturas_fft / W
 
-    # Normalizamos a parte el valor medio, dado que se repite en la muestra 
+    # Normalizamos a parte el valor medio, dado que se repite en la muestra
     h_alturas_fft_normalizadas[0] = h_alturas_fft_normalizadas[0]/2
 
-    # Seleccion de los armonicos
-    maximos = np.flip(np.sort(h_alturas_fft_normalizadas[0:W]))[0:n_armonicos]
+    # Seleccion de los armonicos utilizamos solo la mitad de la muestra
+    mitad = h_alturas_fft_normalizadas[0:W]
+    maximos = np.flip(np.sort(mitad))[0:n_armonicos]
     h_alturas_fft_filtrados = np.where(
-        h_alturas_fft_normalizadas[0:W] < np.min(maximos),
+        mitad < np.min(maximos),
         0,
-        h_alturas_fft_normalizadas[0:W])
+        mitad)
     indices_elementos_filtrados = np.nonzero(h_alturas_fft_filtrados)[0]
 
-    print(maximos, len(h_alturas_fft_filtrados))
-
     # Guardamos los datos de la corrida.
-    store_fft_data(
-        indices_elementos_filtrados,
-        h_alturas_fft,
-        a_alturas_fft,
-        fft_writer
-    )
+    if (save_data is True):
+        store_fft_data(
+            indices_elementos_filtrados,
+            h_alturas_fft,
+            a_alturas_fft,
+            fft_writer
+        )
 
     # Calculo de las alturas.
     sf_alturas = serie_fourier_altura(
@@ -100,7 +106,8 @@ def procesar_rango(alturas, n_armonicos):
         h_alturas_fft_normalizadas,
         a_alturas_fft,
         omega_0,
-        sft_writer
+        sft_writer,
+        save_data
     )
 
     # Error cuadratico medio.
@@ -114,8 +121,8 @@ def procesar_rango(alturas, n_armonicos):
 
 def usage():
     print('\nUso:')
-    print('\tmain.py -p <P> -n <N>')
-    print('\tmain.py --punto <P> --armonicos <N>')
+    print('\tmain.py -p <P> -n <N> -s')
+    print('\tmain.py --punto <P> --armonicos <N> --save')
     print('\nN = cantidad de armonicos (default: 15)')
     print('P = punto del tp (default: el set completo)\n')
     print('Puntos:')
@@ -129,8 +136,10 @@ def usage():
 def main(argv):
     point = 0
     n_armonicos = 15
+    save_data = False
     try:
-        opts, args = getopt.getopt(argv, "hp:n:", ['punto=', 'armonicos='])
+        opts, args = getopt.getopt(argv, "hp:n:s", ['punto=', 'armonicos=', 'save='])
+        print(opts, args)
     except getopt.GetoptError:
         usage()
         sys.exit(2)
@@ -138,6 +147,8 @@ def main(argv):
         if opt == '-h':
             usage()
             sys.exit()
+        elif opt in ("-s", "--save"):
+            save_data = True
         elif opt in ("-p", "--punto"):
             try:
                 value = int(arg)
@@ -157,8 +168,9 @@ def main(argv):
                 usage()
                 sys.exit(2)
 
+    print('Save data', save_data)
     ranges = read_file()
-    procesar_rango(ranges[point], n_armonicos)
+    procesar_rango(ranges[point], n_armonicos, save_data)
 
 
 if __name__ == "__main__":
