63df02349a0c889fa2afdfcb3972fcfffbd204d2
Jorge Moreno <jmoren@gmail.com>
2021-06-16 21:00:09 -0300

    Predictor de mareas - WIP

--- predictor.py
+++ predictor.py
@@ -1,56 +1,92 @@
 import numpy as np
 import pandas
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 mareas = pandas.read_csv('Mar-del-plata.csv')
 alturas = mareas['altura']
-
-T = len(alturas)
-x = range(0, int(T))
-w_0 = 2*np.pi/T
+fechas = mareas['fecha_hora']
 a_0 = np.mean(alturas)
-res = 0
-medidos = []
+
+print(np.amax(alturas))
+alturas_2 = np.where(alturas < a_0, 0, alturas) - a_0
+
+diffs = []
+minutes = []
+format = "%d/%m/%y %H:%M:%S"
+for index, f in enumerate(fechas):
+    if (index == 0):
+        diffs.append(0.0)
+        minutes.append(0.0)
+    else:
+        d0 = datetime.strptime(fechas[index - 1], format)
+        d1 = datetime.strptime(f, format)
+        secs = (d1 - d0).total_seconds()/60
+        next_val = secs + minutes[index - 1]
+        minutes.append(float(next_val))
+        diffs.append(float(secs))
+
+# a = np.matrix([[Q1, Q2], [Q3, Q4]])
+# b = np.array([Y1, Y2])
+# c = np.linalg.solve(a, b)
+#
+# c1 = c[0]
+# c2 = c[1]
+
+# print(f'a_0 = {a_0}, T = {T}, c1 = {c1}, c2 = {c2}')
 
 
-def q1(t):
-    return np.cos(w_0 * t)
+# def f(x):
+#     return (c1 * np.cos(w_0 * x)) + (c2 * np.sin(w_0 * x))
 
 
-def q2(t):
-    return np.sin(w_0 * t)
+# M * C = Y
+Y = alturas
+C = int(np.mean(diffs))
+X = minutes
+
+print(f'alturas: {len(alturas_2)}; minutes: {len(minutes)}')
 
 
-Q1 = sum(q1(idx)*q1(idx) for idx, val in enumerate(alturas))/len(alturas)
-Q2 = sum(q1(idx)*q2(idx) for idx, val in enumerate(alturas))/len(alturas)
-Q3 = sum(q2(idx)*q1(idx) for idx, val in enumerate(alturas))/len(alturas)
-Q4 = sum(q2(idx)*q2(idx) for idx, val in enumerate(alturas))/len(alturas)
+def f_1(x):
+    return np.cos(C*x)
 
-Y1 = sum(value*q1(idx) for idx, value in enumerate(alturas))/len(alturas)
-Y2 = sum(value*q2(idx) for idx, value in enumerate(alturas))/len(alturas)
 
+def f_2(x):
+    return np.sin(C*x)
+
+
+Q1 = sum(f_1(diffs[i])*f_1(diffs[i]) for i, x in enumerate(alturas))
+Q2 = sum(f_1(diffs[i])*f_2(diffs[i]) for i, x in enumerate(alturas))
+Q3 = sum(f_2(diffs[i])*f_1(diffs[i]) for i, x in enumerate(alturas))
+Q4 = sum(f_2(diffs[i])*f_2(diffs[i]) for i, x in enumerate(alturas))
+
+Y1 = sum(alturas_2[i]*f_1(diffs[i]) for i, x in enumerate(alturas))
+Y2 = sum(alturas_2[i]*f_2(diffs[i]) for i, x in enumerate(alturas))
 
 a = np.matrix([[Q1, Q2], [Q3, Q4]])
 b = np.array([Y1, Y2])
+
 c = np.linalg.solve(a, b)
 
-c1 = c[0]
-c2 = c[1]
-
-print(f'a_0 = {a_0}, T = {T}, c1 = {c1}, c2 = {c2}')
+print(c)
 
 
-def f(x):
-    return a_0 + (c1 * np.cos(w_0 * x)) + (c2 * np.sin(w_0 * x))
+# c1 =  B * cos(D) => D = fi_1; B = a_1
+# c2 = -B * sin(D) => D = fi_1; B = a_1
+def g(x):
+    return a_0 + c[0] * np.cos(C*x) + c[1] * np.sin(C*x)
 
 
-for i in x:
-    y_medido = f(i)
-    medidos.append(y_medido)
+medidos = []
+for min in minutes:
+    val = g(min)
+    medidos.append(val)
 
 
-plt.plot(x, alturas, 'r-', x, medidos, 'b--')
+t = range(0, len(alturas))
+plt.plot(t, alturas_2, 'r-', t, medidos, 'g--')
 plt.show()
 
-ecm = np.mean(np.abs(medidos - alturas)**2)
-print('ECM', ecm)
+# ecm = np.mean(np.abs(medidos - alturas)**2)
+# print('ECM', ecm)
